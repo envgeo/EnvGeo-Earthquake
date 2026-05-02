e14d42184d0229c494ee5ea2ea034aadcdd3dbad
Toyoho Ishimura <130520553+envgeo@users.noreply.github.com>
2026-05-02 22:19:09 +0900

    Refactor title and add file metadata
    Updated the title and added metadata in the header.

--- home.py
+++ home.py
@@ -1,1 +1,29 @@
-    st.title('EnvGeo Earthquake (test version)')
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 1 2026
+@author: Toyoho Ishimura @Kyoto-U
+2026/05/01 update
+"""
+
+import streamlit as st
+import re
+from pathlib import Path
+
+
+
+
+def main():
+
+    st.title('EnvGeo Earthquake')
+    st.subheader("An Interactive Platform for Exploring Earthquake Data")
+    st.write('Version 0.1.1 (2026-05-01)')
+
+
+    
+
+    
+if __name__ == '__main__':
+    main()
+    
+    
